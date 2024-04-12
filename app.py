55e1fa0ce4fa9171744f0cd5dfc1731b38607d40
kevinleanderlouis <158525618+kevinleanderlouis@users.noreply.github.com>
2024-04-12 15:07:05 -0400

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ ip_address = socket.gethostbyname(hostname)
 
 @app.route('/')
 def hello_cloud():
-  return 'Hello Cloud!'
+  return 'Wild Ryhdes Company Welcomes You!'
   
 @app.route('/host')
 def host_name():
